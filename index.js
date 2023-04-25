4ce1aacd3cad5f6335ccdc4069a985fa61cd1052
Iryna Zaitseva <zaitseva02@gmail.com>
2023-04-25 19:35:45 +0300

    check functions

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const moment = require('moment');
 const app = express();
+const fs = require('fs/promises')
 
-app.listen(3000, () => console.log("server running"));
+app.use(async (req, res, next) => {
+    const { method, url } = req
+    const date = moment().format('dd-mm-yyyy_hh:mm:ss')
+    await fs.appendFile('server.log', `\n${method} ${url} ${date}`)
+    next()
+})
+app.get('/', (request, response) => {
+    response.send('<h2>Home Home<h2>')
+})
+
+app.use((req, res, next) => {
+    console.log('first middleware');
+    next()
+})
+
+app.listen(3000, () => console.log("server running"))
